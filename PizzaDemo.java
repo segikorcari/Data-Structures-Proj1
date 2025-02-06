1ba83f9d95cebef7a6f48bc8225dbdd0f8baa586
segikorcari <korcari.segi@student.oaklandcc.edu>
2025-02-06 17:41:06 -0500

    Update PizzaDemo.java

--- PizzaDemo.java
+++ PizzaDemo.java
@@ -1,5 +1,9 @@
-PizzaDemo class for testing
-class PizzaDemo {
+package proj1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PizzaDemo {
     public static void main(String[] args) {
         Pizza pizza1 = new Pizza(CrustType.Butter, new ArrayList<>(List.of("cheese", "pepperoni")), SizeType.Medium);
         Pizza pizza2 = new Pizza(CrustType.Garlic, new ArrayList<>(List.of("cheese", "ham", "onion")), SizeType.Large);
@@ -9,7 +13,7 @@ class PizzaDemo {
         System.out.println(pizza2);
         System.out.println(pizza3);
 
-        System.out.println("Pizza1 equals Pizza2: " + pizza1.equals(pizza2));
-        System.out.println("Pizza1 compareTo Pizza2: " + pizza1.compareTo(pizza2));
+        System.out.println("Pizza1 equals Pizza2? " + pizza1.equals(pizza2));
+        System.out.println("Comparing Pizza1 and Pizza2: " + pizza1.compareTo(pizza2));
     }
 }
